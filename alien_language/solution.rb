c0b061b8ad76ee5071904cbac1bd0d4489e593b2
xymind <jogeavi914@gmail.com>
2014-02-07 10:01:08 -0600

    updated to english

--- alien_language/solution.rb
+++ alien_language/solution.rb
@@ -9,34 +9,33 @@ L = numeros[0]
 D = numeros[1]
 N = numeros[2]
 
-diccionario = []
+dictionary = []
 
 for i in (0..D-1) do
 
-  diccionario.push(file.readline.chomp)
+  dictionary.push(file.readline.chomp)
 
 end
 
 
-salida = File.open("r.out","w")
+out = File.open("r.out","w")
 
 for j in (0..N-1)
   line = file.readline.chomp
-  # Expresiones regulares - regex
-  line = line.gsub(/[(]/,"[") #[]
+  line = line.gsub(/[(]/,"[") 
   line = line.gsub(/[)]/,"]")
 
   regex = Regexp.new line
 
-  coincidencias = 0
-  for palabra in diccionario do
-    #palabra  - [asbp]alabra
-    if palabra =~ regex
-      coincidencias+=1
+  match = 0
+  for word in dictionary do
+    #word  - [asbp]alabra
+    if word =~ regex
+      match+=1
     end
   end
-  output = "Case ##{j+1}: #{coincidencias}\n"
-  salida.write(output)
+  output = "Case ##{j+1}: #{match}\n"
+  out.write(output)
 
 end
-salida.close
+out.close
